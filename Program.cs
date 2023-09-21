425939f36152c3137ae577b30730b2dd9cef167d
Ravza <Ravza@DESKTOP-C5R83NG>
2023-09-21 16:29:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] kurslar = new string[] { "Yazılım kampı", "java", "Python" };
+
+            for (int i = 0; i < kurslar.Length; i++) 
+            {
+                Console.WriteLine(kurslar[i]);
+            } 
+            
+            foreach (string kurs in kurslar) 
+            {  
+                
+                Console.WriteLine(kurs); 
+            }
+
+
+        }
+    }
+}
